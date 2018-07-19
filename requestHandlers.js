595fd5d2d049ddd90113ec42d7eed805fcf9bee1
Errol <s20a3264@hotmail.com>
2018-07-19 15:09:52 +0800

    upload img

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,8 @@
 var {exec} = require('child_process');
+var fs = require("fs");
 var {readFile} = require('fs');
 var querystring = require('querystring');
+var formidable = require("formidable");
 
 function home(response) {
   console.log("Request handler 'home' was called.");
@@ -21,19 +23,25 @@ function login(response) {
   response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
   console.log("Request handler 'upload' was called.");
 
-  data = querystring.parse(postData).text
+  var form = new formidable.IncomingForm();
+  console.log('parse ...')
+  form.parse(request, function(error, fields, files) {
+    console.log('parsing done')
+    fs.renameSync(files.upload.path, './tmp/test.png');
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write('received image:<br/>');
+    response.write("<img src='/show'>");
+    response.end();
+  })
 
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write(`you post ${data}`);
-  response.end();
 }
 
-function show(response, postData) {
+function show(response) {
   console.log("Request handler 'show' was called.");
-  readFile('./tmp/clock.png', 'binary', function(error, file) {
+  readFile('./tmp/test.png', 'binary', function(error, file) {
     if(error) {
       response.writeHead(500, {"Content-Type": "text/plain"});
       response.write(`${error}\n`);
